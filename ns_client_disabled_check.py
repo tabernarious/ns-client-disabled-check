467faa216de5905acf16ea9d1a2599973ff0b102
tabernarious <tavdan@gmail.com>
2022-01-28 21:37:45 -0600

    Added per-user granularity and expanded available device information

--- ns_client_disabled_check.py
+++ ns_client_disabled_check.py
@@ -5,9 +5,10 @@
 ## Version:     0.3
 ##################################################
 
+import datetime
 import json
 import socket
-import sys
+#import sys
 import urllib.request, urllib.parse
 import argparse
 from getpass import getpass
@@ -17,7 +18,8 @@ parser = argparse.ArgumentParser(description='Find devices with inactive Netskop
 parser.add_argument('--tenant', type=str, required=True, help='Name of Netksope Tenant: example.goskope.com')
 parser.add_argument('--token_v1', type=str, required=False, help='Netskope APIv1 token. Leave blank to be prompted for token.')
 parser.add_argument('--timeperiod', type=int, required=False, help='How far back (in seconds) to look for correlating events. Default is 86400 (1 day).')
-parser.add_argument('--devicelimit', type=int, required=False, help='Number of devices to fetch that are marked as "disabled" by Netskope. Default is 100. Maximum is 5000.')
+parser.add_argument('--devicelimit', type=int, required=False, help='Number of devices to fetch that are marked as "Disabled" by Netskope. Default is 100. Maximum is 5000.')
+parser.add_argument('--showallusers', action='store_true', help='Lists all users configured for each device. This will also list devices with *any* user marked "Disabled".')
 args = parser.parse_args()
 
 tenant = args.tenant
@@ -37,10 +39,13 @@ if args.devicelimit:
 else:
     device_limit = 100
 
+show_all_users = args.showallusers
+
 print()
-print('Tenant Name:  ', args.tenant)
+print('Tenant Name:  ', tenant)
 print('Time Period:  ', event_timeperiod)
 print('Device Limit: ', device_limit)
+print('ShowAllUsers: ', show_all_users)
 
 
 # Set socket timeout. Default is indefinite, so without API calls will hang the script. 
@@ -48,7 +53,7 @@ socket_timeout = 10 # seconds
 socket.setdefaulttimeout(socket_timeout)
 
 
-def get_devices(tenant, token, device_query, limit=5000):
+def get_devices(tenant, token, device_query, limit=5000, show_all_users=False):
     # Define path for this API endpoint
     api_path = "/api/v1/clients"
 
@@ -69,20 +74,51 @@ def get_devices(tenant, token, device_query, limit=5000):
         response_json = json.loads(response.read().decode("utf-8"))
 
         # Extract list of device hostnames
-        disabled_devices = [devices['attributes']['host_info']['hostname'] for devices in response_json['data']]
-        disabled_devices_response_status = 0 # API responded with data
+        #device_list = [devices['attributes']['host_info']['hostname'] for devices in response_json['data']]
+        device_list = []
+
+        # For each device loop through listed users and create duplicate duplicate device with relevant status
+        #print(response_json['data'])
+        for device_id in response_json['data']:
+            attributes = device_id['attributes']
+
+            for user in attributes['users']:
+                dev_dict = {}
+                dev_dict['device_hostname'] = attributes['host_info']['hostname']
+                dev_dict['device_os'] = attributes['host_info']['os']
+                dev_dict['device_os_version'] = attributes['host_info']['os_version']
+                dev_dict['device_client_version'] = attributes['client_version']
+                dev_dict['device_last_client_status'] = attributes['last_event']['status']
+                dev_dict['device_last_client_event'] = attributes['last_event']['event']
+                #dev_dict['device_client_install_time'] = attributes['client_install_time']
+                try:
+                    dev_dict['device_username'] = user['username']
+                except:
+                    dev_dict['device_username'] = "(no user)"
+                dev_dict['device_user_client_actor'] = user['last_event']['actor']
+                dev_dict['device_user_client_event'] = user['last_event']['event']
+                dev_dict['device_user_client_status'] = user['last_event']['status']
+                dev_dict['device_user_timestamp'] = datetime.datetime.fromtimestamp(user['last_event']['timestamp'])
+
+                device_list.append(dev_dict)
+
+                if show_all_users == False:
+                    break
+
+        device_list_response_status = 0 # API responded with data
+
     except socket.timeout:
         print("ERROR: API timeout for " + api_path)
-        disabled_devices = ""
-        disabled_devices_response_status = 2 # API timed out
+        device_list = ""
+        device_list_response_status = 2 # API timed out
         exit()
     except:
         print("ERROR: No data from " + api_path + " (confirm correct API token)")
-        disabled_devices = ""
-        disabled_devices_response_status = 1 # Could not parse data (likely no data returned or bad token)
+        device_list = ""
+        device_list_response_status = 1 # Could not parse data (likely no data returned or bad token)
         exit()
 
-    return disabled_devices_response_status, disabled_devices
+    return device_list_response_status, device_list
 
 
 def get_events(tenant, token, event_query, event_type="page", timeperiod="86400", limit="1"):
@@ -120,21 +156,44 @@ def get_events(tenant, token, event_query, event_type="page", timeperiod="86400"
     return event_response_status, event_data
 
 
-def find_devices_with_disabled_clients(tenant, token, timeperiod="86400", device_limit="100"):
+def find_devices_with_disabled_clients(tenant, token, timeperiod="86400", device_limit="100", show_all_users=False):
     print()
     print('Getting list of devices with Netskope Client marked as "disabled"...')
+
+    # The query for the clients API that will return "Disabled" devices. The query 'last_event.status eq 0' will return devices where any user's last event was "Disabled", even if the last overall event was "Enabled".
     device_query = "last_event.status eq 0"
-    disabled_devices_response_status, disabled_devices = get_devices(tenant, token, device_query, device_limit)
+
+    # Get a list of devices
+    disabled_devices_response_status, disabled_devices = get_devices(tenant, token, device_query, device_limit, show_all_users)
 
     print(f"Searching for events within the last {timeperiod} seconds that correlate with each device...")
 
     # Print csv headers
     print()
-    print('"device_hostname","event_domain","event_timestamp","result"')
+    print('"device_hostname","device_username","device_os","device_os_version","device_client_version","device_user_client_status","device_user_client_actor","device_user_client_event","device_user_timestamp","event_domain","event_timestamp","result"')
 
-    for device_hostname in disabled_devices:
-        device_type = "unknown" # For the future; trying to pull Device Type from get_devices().
-        event_query = "hostname eq " + device_hostname
+    # Loop through devices returned from get_devices() function
+    for device in disabled_devices:
+        # Skip listing a device if the last status reported by any user was "Enabled". This is required because the query 'last_event.status eq 0' will return devices where any user's last event was "Disabled", even if the last overall event was "Enabled".
+        if device['device_last_client_status'] == "Enabled" and show_all_users == False:
+            continue
+
+        device_hostname = device['device_hostname']
+        device_username = device['device_username']
+        device_os = device['device_os']
+        device_os_version = device['device_os_version']
+        device_client_version = device['device_client_version']
+        device_user_client_actor = device['device_user_client_actor']
+        device_user_client_event = device['device_user_client_event']
+        device_user_client_status = device['device_user_client_status']
+        device_user_timestamp = device['device_user_timestamp']
+        #device_last_client_status = device['device_last_client_status']
+        #device_last_client_event = device['device_last_client_event']
+
+        if device_username == "(no user)":
+            event_query = "hostname eq " + device_hostname
+        else:
+            event_query = "hostname eq " + device_hostname + " and user eq " + device_username
         event_domain = ""
         event_timestamp = ""
         result = ""
@@ -143,16 +202,22 @@ def find_devices_with_disabled_clients(tenant, token, timeperiod="86400", device
         event_response_status, event_data = get_events(tenant, token, event_query, "page", timeperiod, "1")
         if event_response_status == 0:
             event_domain = event_data["domain"]
-            event_timestamp = event_data["timestamp"]
-            result = "OKAY: Events found during set timeperiod"
+            event_timestamp = datetime.datetime.fromtimestamp(event_data["timestamp"])
+            if device_user_client_status == "Disabled":
+                result = "OKAY: Disabled but events found during set timeperiod"
+            else:
+                result = "OKAY: Enabled and events found during set timeperiod"
         elif event_response_status == 1:
-            result = "TROUBLESHOOT: No events found during set timeperiod"
+            if device_user_client_status == "Disabled":
+                result = "TROUBLESHOOT: Disabled and no events found during set timeperiod"
+            else:
+                result = "TROUBLESHOOT: Enabled but no events found during set timeperiod"
         elif event_response_status == 2:
             result = "ERROR: API timed out; try again and or extend the timeout"
         else:
             result = "ERROR: Unknown response from event API"
 
-        print(f'"{device_hostname}","{event_domain}","{event_timestamp}","{result}"')
+        print(f'"{device_hostname}","{device_username}","{device_os}","{device_os_version}","{device_client_version}","{device_user_client_status}","{device_user_client_actor}","{device_user_client_event}","{device_user_timestamp}","{event_domain}","{event_timestamp}","{result}"')
 
 
-devices_to_troubleshoot = find_devices_with_disabled_clients(tenant, token_v1, event_timeperiod, device_limit)
+devices_to_troubleshoot = find_devices_with_disabled_clients(tenant, token_v1, event_timeperiod, device_limit, show_all_users)
